1d531d951d8bec8ab8b6b6bb50d88dae6002776a
NotStancho <jadanstanislav05@gmail.com>
2026-01-14 20:50:35 +0200

    Adding equipment api

--- frontend/src/api/equipment.ts
+++ frontend/src/api/equipment.ts
@@ -1,5 +1,12 @@
+// api/equipment
 import { api } from './api';
-import type { EquipmentFull } from '../types/equipment';
+import type { Equipment } from '../types/equipment/equipment';
+import type { EquipmentFull } from '../types/equipment/equipmentFull';
+
+export const findEquipmentByModelAndSerial = async (modelId: number, serialNumber: string) =>
+    (await api.get<Equipment>('/api/equipment/find', {
+        params: { modelId, serialNumber }
+    })).data;
 
 export const getEquipmentFullById = async (id: number) =>
     (await api.get<EquipmentFull>(`/api/equipment/${id}/full`)).data;
