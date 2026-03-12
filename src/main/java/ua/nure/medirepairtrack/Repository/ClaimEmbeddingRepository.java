87b9810d1f45856f0b8e2386147fe6d04feed9df
NotStancho <jadanstanislav05@gmail.com>
2026-03-12 14:48:53 +0200

    Add method to find ClaimEmbedding by claimId excluding specified ID

--- src/main/java/ua/nure/medirepairtrack/Repository/ClaimEmbeddingRepository.java
+++ src/main/java/ua/nure/medirepairtrack/Repository/ClaimEmbeddingRepository.java
@@ -3,5 +3,8 @@ package ua.nure.medirepairtrack.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ua.nure.medirepairtrack.Entity.ClaimEmbedding.ClaimEmbedding;
 
+import java.util.List;
+
 public interface ClaimEmbeddingRepository extends JpaRepository<ClaimEmbedding, Integer> {
+    List<ClaimEmbedding> findByClaimIdNot(Integer claimId);
 }
