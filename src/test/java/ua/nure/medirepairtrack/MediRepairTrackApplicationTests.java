73b1df586ccfcb14b582b53f522bdcb0fa19bb73
NotStancho <jadanstanislav05@gmail.com>
2026-03-23 13:58:23 +0200

    Refactor test class name to match application naming convention

--- src/test/java/ua/nure/medirepairtrack/MediRepairTrackApplicationTests.java
+++ src/test/java/ua/nure/medirepairtrack/MediRepairTrackApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SoapzPzKnt222ZhadanApplicationTests {
+class MediRepairTrackApplicationTests {
 
     @Test
     void contextLoads() {
