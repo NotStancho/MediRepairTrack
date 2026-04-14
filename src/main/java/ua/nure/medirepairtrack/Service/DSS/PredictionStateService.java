6dd466b1f91b2143dce350744cb44111189c8681
NotStancho <jadanstanislav05@gmail.com>
2026-04-14 22:07:12 +0300

    Update PredictionStateService to change DiagnosisType from AUTOMATED to HYBRID

--- src/main/java/ua/nure/medirepairtrack/Service/DSS/PredictionStateService.java
+++ src/main/java/ua/nure/medirepairtrack/Service/DSS/PredictionStateService.java
@@ -3,6 +3,7 @@ package ua.nure.medirepairtrack.Service.DSS;
 import org.springframework.stereotype.Component;
 import ua.nure.medirepairtrack.Entity.DSS.DiagnosisPrediction.DiagnosisPrediction;
 import ua.nure.medirepairtrack.Entity.DSS.DiagnosisPrediction.PredictionSource;
+import ua.nure.medirepairtrack.Entity.Diagnosis.DiagnosisType;
 
 @Component
 public class PredictionStateService {
@@ -10,6 +11,10 @@ public class PredictionStateService {
     public void markAsHybridIfNeeded(DiagnosisPrediction prediction) {
         if (prediction.getPredictionSource() == PredictionSource.AUTOMATED) {
             prediction.setPredictionSource(PredictionSource.HYBRID);
+        }
+
+        if (prediction.getDiagnosis().getDiagnosisType() == DiagnosisType.AUTOMATED) {
+            prediction.getDiagnosis().setDiagnosisType(DiagnosisType.HYBRID);
         }
     }
 }
